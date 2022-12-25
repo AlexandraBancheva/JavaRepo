6ab1126aaa61d2a1b25d1b9f159e02f94015c637
AlexandraBancheva <sandriche90@gmail.com>
2022-12-25 22:42:40 +0200

    First part of Legendary Farming task

---
+++ LegendaryFarming/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] inputs = scanner.nextLine().split(" ");  // 3 Motes 5 stones 5 Shards
+
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        while (inputs.length > 0){
+
+        }
+
+        scanner.close();
+    }
+}
