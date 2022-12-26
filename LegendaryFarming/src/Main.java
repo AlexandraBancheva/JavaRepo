fe8a8ba2564de18a9d8900dc0f84738da02342a4
AlexandraBancheva <sandriche90@gmail.com>
2022-12-26 21:22:35 +0200

    Second part from Legendary item

--- LegendaryFarming/src/Main.java
+++ LegendaryFarming/src/Main.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,10 +7,40 @@ public class Main {
         String[] inputs = scanner.nextLine().split(" ");  // 3 Motes 5 stones 5 Shards
 
         HashMap<String, Integer> map = new HashMap<String, Integer>();
+        map.put("shards", 0);
+        map.put("fragments", 0);
+        map.put("motes", 0);
+        HashMap<String, Integer> junk = new HashMap<String, Integer>();
+        String legendaryItem = "";
 
-        while (inputs.length > 0){
-
+        while (!inputs.equals("end")){
+            for (int i = 0; i < inputs.length; i++) {
+                int quantity = Integer.parseInt(inputs[i]);
+                String material = inputs[i + 1].toLowerCase();
+                if (material.equals("shards") || material.equals("fragments") || material.equals("motes")) {
+                    map.put(material, map.get(material) + quantity);
+                } else {
+                    if (junk.containsKey(material)) {
+                        junk.put(material, junk.get(material) + quantity);
+                    } else {
+                        junk.put(material, quantity);
+                    }
+                }
+            }
+            inputs = scanner.nextLine().split(" ");
         }
+
+        // Check for legendary item
+        for (String item: map.keySet()) {
+            if (map.get(item) == 250) {
+                legendaryItem = item;
+                map.put(item, map.get(item) - 250);
+                break;
+            }
+        }
+
+        System.out.printf("%s obtained!", legendaryItem);
+        map.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(k -> System.out.println(k.getKey() + ": " + k.getValue()));
 
         scanner.close();
     }
