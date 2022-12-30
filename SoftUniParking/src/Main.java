647bb1b2f2bf168455769382bec4c4d44ca55b7d
AlexandraBancheva <sandriche90@gmail.com>
2022-12-30 11:06:14 +0200

    Added some checks and print the results

--- SoftUniParking/src/Main.java
+++ SoftUniParking/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -7,25 +8,37 @@ public class Main {
         HashMap<String, String> registerLicenseNumbers = new HashMap<String, String>();
         String username = "";
 
-        int numberOfCommands = scanner.nextInt();
-
+        int numberOfCommands = Integer.parseInt(scanner.nextLine());
 
         for (int i = 0; i < numberOfCommands; i++) {
-            String[] inputs = scanner.nextLine().split(" ");
-            if (inputs[0] == "register") {
-                username = inputs[1];
-                String licensePlateNumber = inputs[2];
+            // read input
+            String inputs = scanner.nextLine();
+            String[] input = inputs.split(" ");
+            String registerOrNot = input[0];
+            if (registerOrNot.equals("register")) {
+                username = input[1];
+                String licensePlateNumber = input[2];
+                if (registerLicenseNumbers.containsKey(username)) {
+                    System.out.println(String.format("ERROR: already registered with plate number %s", licensePlateNumber));
+                } else {
+                    registerLicenseNumbers.put(username, licensePlateNumber);
 
-                registerLicenseNumbers.put(username, licensePlateNumber);
-
-                System.out.println(String.format("%s registered %s successfully", username, licensePlateNumber));
-            } else if (inputs[0] == "unregister") {
-                username = inputs[1];
-
-                System.out.println(String.format("%s unregistered successfully", username));
+                    System.out.println(String.format("%s registered %s successfully", username, licensePlateNumber));
+                }
+            } else if (registerOrNot.equals("unregister")) {
+                username = input[1];
+                if (!registerLicenseNumbers.containsKey(username)) {
+                    System.out.println(String.format("ERROR: user %s not found", username));
+                } else {
+                    registerLicenseNumbers.remove(username);
+                    System.out.println(String.format("%s unregistered successfully", username));
+                }
             }
         }
 
+        for (Map.Entry<String, String> user: registerLicenseNumbers.entrySet()) {
+            System.out.println(String.format("%s => %s", user.getKey(), user.getValue()));
+        }
         scanner.close();
     }
 }
