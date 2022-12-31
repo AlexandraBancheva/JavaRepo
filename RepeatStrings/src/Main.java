9fdf9655d0beb08cd3cbfdf7304e28c8a74d0185
AlexandraBancheva <sandriche90@gmail.com>
2022-12-31 16:13:09 +0200

    Repeat string task

---
+++ RepeatStrings/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] words = scanner.nextLine().split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String word: words) {
+            for (int i = 0; i < word.length(); i++) {
+                result.append(word);
+            }
+        }
+        System.out.println(result);
+    }
+}
