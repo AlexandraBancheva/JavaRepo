37029af955a72546614688b910b9d0b083551840
AlexandraBancheva <sandriche90@gmail.com>
2023-01-05 11:33:48 +0200

    Added getters and setters

--- CarInfo/src/Main.java
+++ CarInfo/src/Main.java
@@ -5,10 +5,11 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         Car car = new Car();
-        car.brand = "Toyota";
-        car.model = "Yaris";
-        car.horsePower = 88;
 
-        System.out.println(String.format("The car is: %s %s - %d HP", car.brand, car.model, car.horsePower));
+        car.setBrand("Toyota");
+        car.setModel("Yaris");
+        car.setHorsePower(88);
+
+        System.out.println(String.format("The car is: %s %s - %d HP", car.getBrand(), car.getModel(), car.getHorsePower()));
     }
 }
