d55269a18b94c6c6d8a90e57ca1876cb5666a444
AlexandraBancheva <sandriche90@gmail.com>
2023-01-05 13:15:58 +0200

    Added two constructors.

--- CarInfo/src/Car.java
+++ CarInfo/src/Car.java
@@ -3,6 +3,18 @@ public class Car {
     public String model;
     public int horsePower;
 
+    public Car(String brand){
+        this.brand = brand;
+        this.model = "unknown";
+        this.horsePower = -1;
+    }
+
+    public Car(String brand, String model, int horsePower){
+        this(brand);
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
     public String getBrand() {
         return this.brand;
     }
--- CarInfo/src/Main.java
+++ CarInfo/src/Main.java
@@ -5,15 +5,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int numberOfCars = Integer.parseInt(scanner.nextLine());
+        Car car;
 
         for (int i = 0; i < numberOfCars; i++) {
             String inputs = scanner.nextLine();
             String[] lines = inputs.split(" ");
 
-            Car car = new Car();
-            car.setBrand(lines[0]);
-            car.setModel(lines[1]);
-            car.setHorsePower(Integer.parseInt(lines[2]));
+            if (lines.length == 1){
+                car = new Car(lines[0]);
+            } else {
+                car = new Car(lines[0], lines[1], Integer.parseInt(lines[2]));
+            }
 
             System.out.println(car.carInfo());
         }
