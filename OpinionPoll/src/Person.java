ed06205585e99032a37667122282c95343ec9aa9
AlexandraBancheva <sandriche90@gmail.com>
2023-01-07 15:15:44 +0200

    Remove commented code.

--- OpinionPoll/src/Person.java
+++ OpinionPoll/src/Person.java
@@ -10,16 +10,8 @@ public class Person {
     public String getName() {
         return name;
     }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
+
     public int getAge() {
         return age;
     }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
 }
