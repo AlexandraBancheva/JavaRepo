9f952035cfb200ef6f84f003ef4bf30a0bd2df2b
AlexandraBancheva <sandriche90@gmail.com>
2023-01-09 15:42:09 +0200

    Finished encapsulation task

--- SortByNameAndAge/src/Person.java
+++ SortByNameAndAge/src/Person.java
@@ -1,2 +1,28 @@
 public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s is %d years old.", getFirstName(), getLastName(), getAge());
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
