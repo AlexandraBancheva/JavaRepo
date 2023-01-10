3099c209bd0abb6b2cd3ce8abf8112aa44769eb6
AlexandraBancheva <sandriche90@gmail.com>
2023-01-10 14:09:40 +0200

    Random Array List task

---
+++ RandomArrayList/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String[] args) {
+        RandomArrayList<Integer> randomArrayList = new RandomArrayList();
+        ThreadLocalRandom.current().ints(50)
+                .forEach(randomArrayList::add);
+
+    }
+}
---
+++ RandomArrayList/src/RandomArrayList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomArrayList<T> extends ArrayList<T> {
+    public T getRandomElement(){
+        int index = ThreadLocalRandom.current().nextInt(super.size());
+        return remove(index);
+    }
+}
