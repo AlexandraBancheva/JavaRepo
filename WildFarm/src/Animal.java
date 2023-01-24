99078ed58d2322b4fa5eee823d15c745d39f5528
AlexandraBancheva <sandriche90@gmail.com>
2023-01-24 15:11:39 +0200

    Added some abstrct classes

---
+++ WildFarm/src/Animal.java
@@ -0,0 +1,11 @@
+public abstract class Animal {
+    String animalName;
+    String animalType;
+    Double animalWeight;
+    Integer foodEaten;
+
+    void makeSound() {
+    }
+    void eat(Food) {
+    }
+}
