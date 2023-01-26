3755a9fbc38797e78e5782c1d5cdc54427897436
AlexandraBancheva <sandriche90@gmail.com>
2023-01-26 15:50:18 +0200

    Added a logic in Main class.

--- HotelReservation/src/Main.java
+++ HotelReservation/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split("\\s+");
+
+        PriceCalculator calculator = new PriceCalculator(
+                Double.parseDouble(input[0])
+                , Integer.parseInt(input[1])
+                , Season.valueOf(input[2].toUpperCase())
+                , input[3].equals("SecondVisit") ? DiscountType.SECOND_VISIT : DiscountType.valueOf(input[3].toUpperCase()));
+
+        System.out.printf("%.2f%n", calculator.calculateReservation());
     }
 }
