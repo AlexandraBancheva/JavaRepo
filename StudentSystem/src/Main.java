a6afd42cac0c930edafffa79c900f21ee56e8d70
AlexandraBancheva <sandriche90@gmail.com>
2023-01-26 14:40:44 +0200

    Added logic in Main class.

--- StudentSystem/src/Main.java
+++ StudentSystem/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        StudentSystem studentSystem = new StudentSystem();
+
+        String input;
+        while (!"Exit".equals(input = scanner.nextLine())){
+            String[] command = input.split("\\s+");
+            String output = studentSystem.parseCommand(command);
+            if (output != null){
+                System.out.println(output);
+            }
+        }
     }
 }
